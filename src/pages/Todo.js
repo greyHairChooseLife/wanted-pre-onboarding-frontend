f8eba81a84fecc4862bde2cf60f64b22a0a372ad
sy <greyhairchooselife@gmail.com>
2023-02-08 03:59:30 +0900

    Assignment 6
    API에 post request 날려서 todo 저장하기

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -8,11 +8,13 @@ const API = 'https://pre-onboarding-selection-task.shop/';
 const Todo = () => {
     const navigate = useNavigate();
     const [todos, setTodos] = useState();
+    const [newTodo, setNewTodo] = useState('');
+
+    const myToken = localStorage.getItem('access_token');
 
     useEffect(() => {
         if (!isLogin()) navigate('/signin');
         else {
-            const myToken = localStorage.getItem('access_token');
             const config = {
                 headers: {
                     Authorization: `Bearer ${myToken}`,
@@ -26,6 +28,26 @@ const Todo = () => {
         }
     }, []);
     console.log('responsed todos: ', todos);
+
+    const onClickNewTodo = () => {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${myToken}`,
+                'Content-Type': 'application/json',
+            },
+        };
+        axios
+            .post(
+                API + 'todos',
+                {
+                    todo: newTodo,
+                },
+                config
+            )
+            .then((res) => console.log(res));
+    };
+
+    const onChangeNewTodoInput = (e) => setNewTodo(e.target.value);
 
     return (
         <>
@@ -43,8 +65,17 @@ const Todo = () => {
                 </label>
             </li>
             <div>
-                <input data-testid="new-todo-input" />
-                <button data-testid="new-todo-add-button">추가</button>
+                <input
+                    data-testid="new-todo-input"
+                    value={newTodo}
+                    onChange={onChangeNewTodoInput}
+                />
+                <button
+                    data-testid="new-todo-add-button"
+                    onClick={onClickNewTodo}
+                >
+                    추가
+                </button>
             </div>
         </>
     );
