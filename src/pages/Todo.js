e144c41f3224e1ab15b142b8f62ffcc68d205273
sy <greyhairchooselife@gmail.com>
2023-02-08 02:55:57 +0900

    Assignment 5 (1/2)
    API + todos 경로로 get 요청하기
    
    ps. 받아 올 데이터가 있을 때 화면에 표시하는 부분을 마저 개발

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -1,12 +1,30 @@
 import { isLogin } from '../utils/total';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const API = 'https://pre-onboarding-selection-task.shop/';
 
 const Todo = () => {
     const navigate = useNavigate();
+    const [todos, setTodos] = useState();
+
     useEffect(() => {
         if (!isLogin()) navigate('/signin');
+
+        const myToken = localStorage.getItem('access_token');
+        const config = {
+            headers: {
+                Authorization: `Bearer ${myToken}`,
+            },
+        };
+
+        axios
+            .get(API + 'todos', config)
+            .then((res) => setTodos(res.data))
+            .catch((err) => console.log(err));
     }, []);
+    console.log('responsed todos: ', todos);
 
     return (
         <>
