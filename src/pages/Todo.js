8bcf2c558c8b82fea79a6ca05ac8cc212627e95b
sy <greyhairchooselife@gmail.com>
2023-02-09 23:28:18 +0900

    Assignment 8
    todo component,
    
    각 todo의 우측에 수정, 삭제 버튼 생성

--- src/pages/Todo.js
+++ src/pages/Todo.js
@@ -75,6 +75,8 @@ const Todo = () => {
                             />
                             <span>{e.todo}</span>
                         </label>
+                        <button data-testid="modify-button">수정</button>
+                        <button data-testid="delete-button">삭제</button>
                     </li>
                 );
             })}
