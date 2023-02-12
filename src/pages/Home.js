6475f24ea9e4ce5e24cc80fb0b0511cd07a9804c
sy <greyhairchooselife@gmail.com>
2023-02-12 09:12:41 +0900

    첫화면 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 const Home = () => {
     return (
         <>
-            <div>hello world!</div>
+            <h1>원티드 프리온보딩 프론트엔드 - 선발과제</h1>
+            <div>안녕하세요!</div>
         </>
     );
 };
